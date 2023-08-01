7f9c5b35c100d188b4c6f23da72f4e3605d4d10c
Jobayer <jobayerhossain977@gmail.com>
2023-08-01 10:44:11 +0600

    feat(IMC): :zap: In memory cache

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
+
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { Module } from '@nestjs/common';
+
+@Module({
+  imports: [
+    CacheModule.register({
+      ttl: 6000,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
+})
+export class AppModule {}
---
+++ src/app.service.ts
@@ -0,0 +1,28 @@
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Inject, Injectable } from '@nestjs/common';
+import axios from 'axios';
+import { Cache } from 'cache-manager';
+
+@Injectable()
+export class AppService {
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
+
+  async getUsers() {
+    try {
+      // const checkCache = await this.cacheManager.get('entries');
+      // if (checkCache) {
+      //   console.log('from cache');
+      //   return checkCache;
+      // }
+      const response = await axios
+        .get('https://api.publicapis.org/entries')
+        .then((res) => res.data);
+
+      // await this.cacheManager.set('entries', response, 60000);
+      console.log('from api');
+      return response;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
