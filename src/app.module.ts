797e61f975ea76ad6a1cb54b397f55b34141cdcf
Jobayer <jobayerhossain977@gmail.com>
2023-08-04 10:29:29 +0600

    staging: Added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CacheModule } from '@nestjs/cache-manager';
+import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [],
+  imports: [CacheModule.register()],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Inject, Injectable } from '@nestjs/common';
+import axios from 'axios';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(@Inject(CACHE_MANAGER) private readonly cacheManager: Cache) {}
+
+  async getHello() {
+    const response = await axios
+      .get('https://api.publicapis.org/entries')
+      .then((res) => res.data);
+    return response;
   }
 }
